0769ed6e0eec45fba5e1f816efdad34b6a1d1173
mazey13 <maisiecolyer@hotmail.com>
2022-08-02 21:59:08 +0100

    Added missing extensions, added social icons

--- greenair-website/src/App.js
+++ greenair-website/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import InstagramIcon from '@mui/icons-material/Instagram.js';
+import WhatsAppIcon from '@mui/icons-material/WhatsApp.js';
+import FacebookIcon from '@mui/icons-material/Facebook.js';
+import TelegramIcon from '@mui/icons-material/Telegram.js';
 
 function App() {
   return (
@@ -17,6 +21,11 @@ function App() {
         >
           Learn React
         </a>
+
+        <FacebookIcon />
+        <TelegramIcon />
+        <WhatsAppIcon />
+        <InstagramIcon />
       </header>
     </div>
   );
