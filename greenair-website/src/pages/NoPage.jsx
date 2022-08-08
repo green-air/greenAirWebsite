5aefc67d4627f400a47839505bbe33c2e8d43346
yusuf3a50 <ybyphone@disroot.org>
2022-08-08 16:35:13 +0100

    netlify failed to deploy due to: 4:29:50 PM: src/NavBar.jsx 4:29:50 PM:   Line 2:10:  'BrowserRouter' is defined but never used  no-unused-vars 4:29:50 PM:   Line 2:25:  'Route' is defined but never used          no-unused-vars 4:29:50 PM: src/pages/NoPage.jsx 4:29:50 PM:   Line 8:13:   img elements must have an alt prop, either with meaningful text, or an empty string for decorative images  jsx-a11y/alt-text 4:29:50 PM:   Line 13:39:  Anchors must have content and the content must be accessible by a screen reader                            jsx-a11y/anchor-has-content
    FIXED these issues (hopefully)

--- greenair-website/src/pages/NoPage.jsx
+++ greenair-website/src/pages/NoPage.jsx
@@ -3,15 +3,12 @@ import './NoPage.css';
 
 function NoPage() {
     return (
-        <div>
           <div class="container">
-            <img class="img" src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Nicosia_01-2017_img07_cats.jpg" width="900px" />
+            <img class="img" src="https://upload.wikimedia.org/wikipedia/commons/1/1f/Nicosia_01-2017_img07_cats.jpg" width="900px" alt="a cat scratching somoenes car tyres"/>
             <div class="center">
               <p class="bubble speech">Meow 404!: Give me cat bizcits meow or I slash your tyres!</p>
             </div>
           </div>
-            <footer>Image courtesy of <a href="https://commons.wikimedia.org/wiki/Main_Page" />Wikimedia Commons</footer>
-        </div>
     );
 }
 
