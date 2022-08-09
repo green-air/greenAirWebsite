49831c1b544a5e175e764c605923ff3626815458
yusuf3a50 <ybyphone@disroot.org>
2022-08-09 12:25:44 +0100

    some arbitrary improvements

--- greenair-website/src/pages/Home.jsx
+++ greenair-website/src/pages/Home.jsx
@@ -4,9 +4,20 @@ import './Home.css';
 function Home() {
     return (
         <div>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Under-Construction-Bulldozer.gif" alt="bulldozer gif" width="100%" />
-            <h1 id="elem">Please be patient, this project website is under construction!</h1>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Under-construction5.gif" alt="under construction gif" width="100%" />
+            <h1 
+                align="center"
+                id="blinking">
+                    Please be patient, this project website is...
+                </h1>
+            <img 
+                src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Under-Construction-Bulldozer.gif" 
+                alt="bulldozer gif" 
+                width="100%" />
+            <img 
+                align="center"
+                width="100%"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Under-construction5.gif" 
+                alt="under construction gif" />
         </div>
     );
 }
