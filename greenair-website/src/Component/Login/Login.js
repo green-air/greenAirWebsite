77a70fb501ada8992f3ace79a7a6111f1d7f3f8a
kamba-ddie <kb531@sussex.ac.uk>
2022-08-09 23:23:28 +0100

    forgot to save all files!

---
+++ greenair-website/src/Component/Login/Login.js
@@ -0,0 +1,32 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+import Icons from '../../Components/Socials_component/Socials.jsx';
+
+import React from 'react';
+
+
+
+function Login() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is the log in page!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <Icons />
+        </header>
+    </div>
+  );
+}
+
+export default Login;
