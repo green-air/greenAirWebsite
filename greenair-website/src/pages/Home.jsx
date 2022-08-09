a338fbf184586a01eea1565102029489d3b9144c
yusuf3a50 <ybyphone@disroot.org>
2022-08-09 10:40:38 +0100

    remove package.json override as this was causing a deploy fail with netlify

--- greenair-website/src/pages/Home.jsx
+++ greenair-website/src/pages/Home.jsx
@@ -4,9 +4,9 @@ import './Home.css';
 function Home() {
     return (
         <div>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Under-Construction-Bulldozer.gif" alt="bulldozer gif" width="100%" />
-        <h1 id="elem">Please be patient, this project website is under construction!</h1>
-        <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Under-construction5.gif" alt="under construction gif" width="100%" />
+            <img src="https://upload.wikimedia.org/wikipedia/commons/1/1c/Under-Construction-Bulldozer.gif" alt="bulldozer gif" width="100%" />
+            <h1 id="elem">Please be patient, this project website is under construction!</h1>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Under-construction5.gif" alt="under construction gif" width="100%" />
         </div>
     );
 }
