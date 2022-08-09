cd8917f056ccd44fbca44aa568f53885677af5d2
yusuf3a50 <ybyphone@disroot.org>
2022-08-09 13:47:58 +0100

    Revert "Merge branch 'main' into topic/-changes"
    This reverts commit c794ccaeee0053ce1a2996a8e8402838805e3832, reversing
    changes made to e20318bc67d696b3981ad07d8d4da759e9631966.

--- greenair-website/src/App.js
+++ greenair-website/src/App.js
@@ -1,58 +1,47 @@
 // import logo from './logo.svg';
-// import './App.css';
-// import { Icons } from './Socials_component/Socials.jsx';
-// import React from 'react';
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//         <Icons />
-//         </header>
-//     </div>
-//   );
-// }
-//export default App;
-// attempt 2 -----------------------------------------------------
-// Navbar functional imports: -------------------------------------------------
-import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout.jsx";
-import Home from "./pages/Home.jsx";
-import Forecast from "./pages/Forecast.js";
-import About from "./pages/About.jsx";
-import NoPage from "./pages/NoPage.jsx";
-// end navbar functional imports ----------------------------------------------
-// Navbar style imports: -------------------------------------------------
-import PrettyNavBar from "./Navigation/PrettyNavBar.js";
-import "bootstrap/dist/css/bootstrap.min.css";
-// end Navbar style imports: -------------------------------------------------
-export default function App() {
+import './App.css';
+import Icons from './Socials_component/Socials.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, {useState} from 'react';
+import {
+  BrowserRouter as Router, Link, Route, Routes
+} from "react-router-dom"
+import Contact from './contact';
+import Home from './Home';
+
+
+
+function App() {
   return (
-  <div>
-    <PrettyNavBar />
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/Forecast" element={<Forecast />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Home" element={<Home />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Router>
+    <div className="App">
+      <header className="App-header">
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+        
+        {/* <Link to='/contact' >Contact Us</Link> */}
+        {/* <Icons /> */}
+        <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/contact" element={<Contact />}/>
+           </Routes>
+          
+        
+        </header>
     </div>
+    </Router>
   );
 }
+
+export default App;
