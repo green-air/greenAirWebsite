59d78e4dcf2598e85d41348c6b5ffaa47af4cd7e
mazey13 <92333806+mazey13@users.noreply.github.com>
2022-08-12 17:34:11 +0100

    added icons to about page

--- greenair-website/src/pages/About.jsx
+++ greenair-website/src/pages/About.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import Icons from "../Socials_component/Socials.jsx"
 
 function About() {
     return (
+        <div>
         <h1>this is the about page</h1>
+        <Icons />
+        </div>
     );
 }
 
