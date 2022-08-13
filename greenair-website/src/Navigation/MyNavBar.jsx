e6ca2e57e36dd7631b10d6ec772d729284eefbe3
yusuf3a50 <ybyphone@disroot.org>
2022-08-13 18:48:18 +0100

    Got one api call from a web form working first: input gps co-ordinates and the forecast api returns forecast data from that location

--- greenair-website/src/Navigation/MyNavBar.jsx
+++ greenair-website/src/Navigation/MyNavBar.jsx
@@ -1,10 +1,10 @@
+import Container from "react-bootstrap/Container";
 import Button from 'react-bootstrap/Button';
-import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Offcanvas from 'react-bootstrap/Offcanvas';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 
 import React from "react";
 import { Link } from "react-router-dom";
