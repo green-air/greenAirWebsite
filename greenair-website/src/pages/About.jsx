0b99b2b9b3918c2c16d5ade84c621b7ffab0f6e4
kamba-ddie <kb531@sussex.ac.uk>
2022-08-14 17:27:08 +0100

    got icons working
    woo!

--- greenair-website/src/pages/About.jsx
+++ greenair-website/src/pages/About.jsx
@@ -19,7 +19,7 @@ function  About () {
 We are professionals dedicated to improving the health and quality of life for billions of people worldwide, by providing the world with the most accurate and actionable Air-Quality data .
 let's together rebuild our world to be better, 
 let's make this earth green with various trees 
-and plants that are around us for the sake of this. 
+and plants that are around us. 
 So that for the sake of this success, start with small things first, let's plant together, reforest, reforest 
 and so on for comfort. our earth let's protect our earth</p></div>
 <img src={image} width="700" height="300" style={{position:'relative', left:'10px'}}></img>
@@ -30,7 +30,7 @@ and so on for comfort. our earth let's protect our earth</p></div>
 <div style={{padding:'40px'}}>
 <h1 style={{textAlign:'center', fontFamily:'Jazz LET, fantasy'}}>Take the Pledge. 
 Be the Change</h1>
-<p style={{textAlign:'center', maxWidth: '50%', position:'relative', left:'180px', fontFamily:'Georgia, serif', fontWeight: 'bold'}}>A car-Free day encourages motorists to give up their cars for a day. Orgaied events are held in som citites and countries. September 22nd is World Car Free Day. According to the washington post, this event promotes improvement of mass transit, cycling and walking, and the development of communities where jobs are closer to home and where shoppping is within walking distance. Studies showed that for short trips in cities, one can reach more quickly using a bicycle rather than using a car.</p></div>
+<p style={{textAlign:'center', maxWidth: '50%', position:'relative', left:'180px', fontFamily:'Georgia, serif', fontWeight: 'bold'}}>A car-Free day encourages motorists to give up their cars for a day. Organised events are held in some citites and countries. September 22nd is World Car Free Day. According to the washington post, this event promotes improvement of mass transit, cycling and walking, and the development of communities where jobs are closer to home and where shoppping is within walking distance. Studies showed that for short trips in cities, one can reach more quickly using a bicycle rather than using a car.</p></div>
 <img src={require('../images/world car free pic.png')} width="400" height="300" style={{position:'relative', left:'300px'}}></img>
 </div>
 
--- greenair-website/src/pages/Login.js
+++ greenair-website/src/pages/Login.js
@@ -1,6 +1,10 @@
 import React, { useState, } from "react";
 import "../pages/login.css"
 import LoginForm from "./LoginForm.js"
+import "../App.js"
+import Icons from "../Socials_component/Socials.jsx"
+import background from"../images/Homepage.jpg";
+import image from '../images/logo(light).png'
 
 function Login () {
     const adminUser = {
@@ -9,6 +13,7 @@ function Login () {
     }
     const [user, setUser] = useState({name:'"', email:""});
     const [error, setError]= useState("");
+
 
 
     
@@ -23,13 +28,19 @@ function Login () {
         setUser({name:"", email:""});
     }
     return (
-        <div className= "App">
-            {(user.email !=="") ? (
-             <div className="welcome"><h2> Welcome,<span>{user.name}</span></h2>
-             <button onClick={Logout}>Logout</button>
+         <div className= "App">
+           {/* {(user.email !=="") ? (
+        //      <div className="welcome"><h2> Welcome,<span>{user.name}</span></h2>
+        //      <button onClick={Logout}>Logout</button>
+        // </div>
+        // ) : ( */}
+             <LoginForm Login={Login} error ={error} 
+             /> 
+<div className="socials">
+        <Icons />
         </div>
-        ) : ( <LoginForm Login={Login} error ={error}/> 
-        )}
+        
         </div>)
-}
+        
+    }
 export default Login; 
