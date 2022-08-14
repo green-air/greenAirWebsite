23084bd48fe204377b874db104c413e94870fd9a
mazey13 <92333806+mazey13@users.noreply.github.com>
2022-08-14 10:54:11 +0100

    Tidying navbar code

--- greenair-website/src/Navigation/MyNavBar.jsx
+++ greenair-website/src/Navigation/MyNavBar.jsx
@@ -52,20 +52,12 @@ function MyNavBar() {
                     Sign-up
                     </NavDropdown.Item>
                     <NavDropdown.Divider />
-                    {/* <NavDropdown.Item href="#action5">
-                      Something else here
-                    </NavDropdown.Item> */}
+                    
                   </NavDropdown>
                 </Nav>
                 
                 <Form className="d-flex">
-                  {/* <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  /> */}
-                  {/* <Button variant="outline-success">Search</Button> */}
+                  
                 </Form>
                 
               </Offcanvas.Body>
